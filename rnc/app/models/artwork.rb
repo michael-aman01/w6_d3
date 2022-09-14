13fbc9548f602c698be44957e9735a2917c3ea65
shihyulung <shihyulung@yahoo.com.tw>
2022-09-14 15:26:06 -0700

    add has_many to artworrk/ users

--- rnc/app/models/artwork.rb
+++ rnc/app/models/artwork.rb
@@ -1,4 +1,7 @@
 class Artwork < ApplicationRecord
   validates :title, uniqueness {scope: :artist_id, message: "should have only one artist_id per title"}
-  belongs_to :artist_id
+
+  belongs_to :artist,
+    foreign_key: :artist_id
+    class_name: :User
 end
--- rnc/app/models/user.rb
+++ rnc/app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    validates presence: true, uniqueness: true 
+    validates presence: true, uniqueness: true
+
+    has_many :artworks,
+        foreign_key: :artist_id,
+        class_name: :Artwork,
+        dependent: :destroy
+
+        
 end
