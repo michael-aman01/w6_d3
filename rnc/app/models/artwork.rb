c066ffd6ca19157371d0b70808db9b86f6d32092
shihyulung <shihyulung@yahoo.com.tw>
2022-09-14 16:00:52 -0700

    add constraints and validations for artwork_share

--- rnc/app/models/artwork.rb
+++ rnc/app/models/artwork.rb
@@ -1,7 +1,16 @@
 class Artwork < ApplicationRecord
-  validates :title, uniqueness {scope: :artist_id, message: "should have only one artist_id per title"}
+  validates :title, presence:true, uniqueness {scope: :artist_id, message: "should have only one artist_id per title"}
+  validates :image_url presence:true
+  validates :artist_id presence:true
+
+  has_many :artwork_shares,
+    dependent: :destroy
 
   belongs_to :artist,
     foreign_key: :artist_id
     class_name: :User
+
+  has_many :artwork_share_viewer,
+    through: :artwork_shares,
+    source: :viewer
 end
