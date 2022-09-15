f22d4a7e5385da3c0f74ccce0ee0a306834d8762
shihyulung <shihyulung@yahoo.com.tw>
2022-09-15 11:14:36 -0700

    add create and show in user_controller

--- rnc/app/controllers/users_controller.rb
+++ rnc/app/controllers/users_controller.rb
@@ -2,12 +2,18 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
+        render json: @users
     end
 
     def create
-        @user = User.create!(user_params)
-        if @user.save
-            
+        @users = User.create!(user_params)
+        if @users.save
+            redirect_to user_url(@users)
+            #redirect_to :action => "index"
+        else
+            render json: @users.errors.full_messages, status: :unprocessable_entity
+        end
+
     end
 
     def update
@@ -17,6 +23,8 @@ class UsersController < ApplicationController
     end
 
     def show
+        @user = User.find(params[:id])
+        render json: @user
     end
 
     def edit
@@ -28,7 +36,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permits(:username)
+        params.require(:user).permit(:username)
     end
 
 
